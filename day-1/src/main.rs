6a0ac4c3a4aa3c2c8e7a63ad71f39b658ef3bf61
Prodigeris <arnas.kazlauskas@refurbed.com>
2024-12-04 22:20:11 +0200

    feat(day-1): second part

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,4 +1,6 @@
+use std::collections::HashMap;
 use std::fs;
+use std::ops::AddAssign;
 
 fn main() {
     let contents = fs::read_to_string("input.txt")
@@ -9,20 +11,35 @@ fn main() {
     let mut left: Vec<u32> = Vec::new();
     let mut right: Vec<u32> = Vec::new();
 
+    let mut occurences: HashMap<u32, u32> = HashMap::new();
+
     for line in lines {
         let mut locations = line.split("   ");
         left.push(locations.next().unwrap().parse::<u32>().unwrap());
-        right.push(locations.next().unwrap().parse::<u32>().unwrap());
+
+        let right_element = locations.next().unwrap().parse::<u32>().unwrap();
+        right.push(right_element);
+
+        if ! occurences.contains_key(&right_element) {
+            occurences.insert(right_element, 1);
+        } else {
+            occurences.get_mut(&right_element).unwrap().add_assign(1);
+        }
     }
 
     left.sort();
     right.sort();
 
     let mut distance = 0;
+    let mut score = 0;
 
     for i in 0..left.len() {
         distance += left[i].abs_diff(right[i]);
+        if occurences.contains_key(&left[i]) {
+            score += left[i] * occurences[&left[i]];
+        }
     }
 
     println!("Distance: {}", distance);
+    println!("Score: {}", score);
 }
